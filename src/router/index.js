177d89bdb9006b41c268885c828fdbc94ef23804
Ruben Del Rio <me@rdelrio.com>
2022-10-14 08:27:48 -0400

    feat(router): :truck: add routing configuration
    includes type checking for VSCode through JSDoc comment syntax

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
+// Let's import TypeScript definitions for VS Code assistance
+/** @ts-check */
+/** @typedef { import('vue-router').RouteConfig } RouteConfig */
+/** @type {RouteConfig[]} */
 const routes = [
   {
     path: "/",
@@ -14,6 +18,45 @@ const routes = [
     name: "Products",
     component: () => import("../views/Products.vue"),
   },
+  {
+    path: "/product",
+    children: [
+      {
+        path: ':id',
+        name: 'Product',
+        children: [
+          {
+            path: '',
+            name: 'ProductPage',
+            component: () => import("../views/Product.vue"),
+          }
+        ]
+      }
+    ],
+
+    // Redirect when no product is specified
+    redirect: { name: 'Products' },
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import("../views/Cart.vue"),
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: () => import("../views/Checkout.vue"),
+  },
+  {
+    path: '/thank-you',
+    name: 'Confirmation',
+    component: () => import("../views/Confirmation.vue"),
+  },
+  {
+    path: '/:notFound(.*)',
+    name: 'NotFound',
+    component: () => import("../views/NotFound.vue"),
+  }
 ];
 
 const router = new VueRouter({
