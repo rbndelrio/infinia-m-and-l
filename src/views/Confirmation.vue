ab08357c21155debee930f985148b9eb67196ce1
Ruben Del Rio <me@rdelrio.com>
2022-10-14 19:02:50 -0400

    feat(pages): :sparkles: add confirmation page

--- src/views/Confirmation.vue
+++ src/views/Confirmation.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    Hi from <span class="italic">Confirmation.vue</span>
+  <div class="bg-white">
+    <div class="mx-auto max-w-3xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+      <div class="max-w-xl">
+        <h1 class="text-base font-medium text-red-600">Thank you!</h1>
+        <p class="mt-2 text-4xl font-bold tracking-tight sm:text-5xl">It's on the way!</p>
+        <p class="mt-2 text-base text-gray-500">Your order #14034056 has shipped and will be with you soon.</p>
+
+        <dl class="mt-12 text-sm font-medium">
+          <dt class="text-gray-900">Tracking number</dt>
+          <dd class="mt-2 text-red-600">51547878755545848512</dd>
+        </dl>
+      </div>
+    </div>
   </div>
 </template>
