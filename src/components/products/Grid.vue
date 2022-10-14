e301d56a5b3987305fe8b1d4ada75dcb23e7e924
Ruben Del Rio <me@rdelrio.com>
2022-10-14 16:59:36 -0400

    fix(product): :bug: fix the grid data format

--- src/components/products/Grid.vue
+++ src/components/products/Grid.vue
@@ -3,20 +3,20 @@
     <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         <div v-for="product in $props.products" :key="product.id" class="group relative">
-          <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-            <img :src="product.imageSrc" :alt="product.imageAlt" class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+          <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-100 group-hover:opacity-75 lg:aspect-none lg:h-80">
+            <img :src="product.thumbnail" :alt="product.description" class="h-full w-full object-cover object-center lg:h-full lg:w-full" loading="lazy" />
           </div>
           <div class="mt-4 flex justify-between">
             <div>
               <h3 class="text-sm text-gray-700">
-                <a :href="product.href">
+                <router-link :to="{ name: 'Product', params: { id: product.id } }">
                   <span aria-hidden="true" class="absolute inset-0" />
-                  {{ product.name }}
-                </a>
+                  {{ product.title }}
+                </router-link>
               </h3>
-              <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+              <p class="mt-1 text-sm text-gray-500" v-if="product.brand">{{ product.brand }}</p>
             </div>
-            <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
+            <p class="text-sm font-medium text-gray-900" v-if="product.price">${{ product.price }}</p>
           </div>
         </div>
       </div>
