985d8d52a80ac2a1d3ef9278cc79510b0f6d7191
Ruben Del Rio <me@rdelrio.com>
2022-10-14 11:30:12 -0400

    feat(state): :building_construction: add cart state management

--- src/store/index.js
+++ src/store/index.js
@@ -8,17 +8,18 @@ const store = new Vuex.Store({
     // User login and browser state
     user: {
       lang: 'en-US',
+      currency: 'USD',
       loggedIn: false,
       name: '',
     },
 
     // Product data cache
-    product: {},
+    product: {/* [id]: { data: ...jsonData } */},
 
     // Product Cart
     cart: {
-      currency: 'USD',
-      products: [],
+      status: null,
+      items: [/* { id: 0, qty: 1 } */],
     },
 
     // Search + Filter state
@@ -43,9 +44,46 @@ const store = new Vuex.Store({
 
   getters: {},
 
-  mutations: {},
+  mutations: {
+    /**
+     * Cart
+     */
+    setCartItem: (state, payload = {}) => {
+      let idx = state.cart.items.findIndex(item => item.id === payload.id)
+      if (idx) state.cart.items[idx] = payload
+      else state.cart.items.push({ id: null, qty: 1, ...payload})
+    },
+    setCartItems: (state, { items = [] }) => {
+      state.cart.items = items
+    },
+    setCartStatus: (state, status = null) => {
+      state.cart.status = status
+    }
+  },
 
-  actions: {},
+  actions: {
+    /**
+     * Cart
+     */
+    addToCart: ({ commit, state }, payload) => {
+      commit('addCartItem', payload)
+    },
+    async checkout ({ commit, state }, products) {
+      const savedCartItems = [...state.items]
+      commit('setCartStatus', null)
+      // empty cart
+      commit('setCartItems', { items: [] })
+      try {
+        await new Promise(res => setTimeout(res, 500))
+        commit('setCartStatus', 'successful')
+      } catch (e) {
+        console.error(e)
+        commit('setCartStatus', 'failed')
+        // rollback to the cart saved before sending the request
+        commit('setCartItems', { items: savedCartItems })
+      }
+    },
+  },
 });
 
 export { store as default };
