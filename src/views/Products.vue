b432e4ede9178dfe7cf0a2396210370e6e3db90b
Ruben Del Rio <me@rdelrio.com>
2022-10-14 16:28:36 -0400

    feat(product): :sparkles: finally add a product grid component

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    Hi from <span class="italic">Products.vue</span>
+    <transition>
+      <Grid :products="productList" />
+    </transition>
   </div>
 </template>
+
+<script>
+
+import Grid from '../components/products/Grid.vue';
+export default {
+  components: {
+    Grid
+  },
+  beforeMount() {
+    const ctx = this
+    this.$store.dispatch('fetchProducts')
+  },
+  computed: {
+    productsCache() { console.log('products!', this.$store.state.product); return this.$store.state.product },
+    productList() { return this.$store.state.page.products?.data }
+  },
+}
+</script>
