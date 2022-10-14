2f70a342d44a5b9561b590f298d1ca09e7c17c7b
Ruben Del Rio <me@rdelrio.com>
2022-10-14 09:05:14 -0400

    build(vite): :wrench: set dummyjson as default host, allow env variables to override

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 import vue from "@vitejs/plugin-vue2";
 import path from "path";
@@ -8,19 +8,26 @@ const filename = fileURLToPath(import.meta.url);
 
 const dirname = path.dirname(filename);
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        // eslint-disable-next-line no-undef
+        "@": path.resolve(dirname, "./src"),
+      },
     },
-  },
-  lintOnSave: true,
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      exclude: ["node_modules/lodash-es/**"],
+    define: {
+      API_HOST: env?.APP_HOST || '"https://dummyjson.com"'
     },
-  },
+    lintOnSave: true,
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true,
+        exclude: ["node_modules/lodash-es/**"],
+      },
+    },
+  }
 });
