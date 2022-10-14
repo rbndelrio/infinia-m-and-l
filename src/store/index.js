8b2e40d65dc93f6291385488af6305d445a31c96
Ruben Del Rio <me@rdelrio.com>
2022-10-14 09:36:31 -0400

    feat(state): :building_construction: Establish shape of store

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,42 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {},
+  state: {
+    // User login and browser state
+    user: {
+      lang: 'en-US',
+      loggedIn: false,
+      name: '',
+    },
+
+    // Product data cache
+    product: {},
+
+    // Product Cart
+    cart: {
+      currency: 'USD',
+      products: [],
+    },
+
+    // Search + Filter state
+    search: {
+      query: '',
+      filter: {
+        price_max: null,
+        price_min: null,
+        rating_min: null,
+        brand: [],
+        category: [],
+      },
+    },
+
+    // Page content cache
+    page: {
+      products: {
+        order: [],
+      },
+    },
+  },
 
   getters: {},
 
